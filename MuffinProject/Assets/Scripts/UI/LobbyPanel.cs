45b933ac12fe7ea9377f7aacdf1a3017ddf4c506
imchanghong <imchanghong15@gmail.com>
2026-05-09 15:39:30 +0900

    Update LobbyPanel.cs

--- MuffinProject/Assets/Scripts/UI/LobbyPanel.cs
+++ MuffinProject/Assets/Scripts/UI/LobbyPanel.cs
@@ -11,28 +11,18 @@ public class LobbyPanel : MonoBehaviour
     [SerializeField] private Button createRoomButton;
     [SerializeField] private Button joinRoomButton;
     
-    [SerializeField] private TMP_InputField nicknameInput;
-    [SerializeField] private Button confirmButton;
-    
     [SerializeField] private GameObject joinRoomPanel;
-
-    private void Start()
-    {
-        UpdateNicknameUI();
-    }
 
     private void OnEnable()
     {
         createRoomButton.onClick.AddListener(OnCreateRoomClicked);
         joinRoomButton.onClick.AddListener(OnJoinRoomClicked);
-        confirmButton.onClick.AddListener(OnConfirmClicked);
     }
 
     private void OnDisable()
     {
         createRoomButton.onClick.RemoveListener(OnCreateRoomClicked);
         joinRoomButton.onClick.RemoveListener(OnJoinRoomClicked);
-        confirmButton.onClick.RemoveListener(OnConfirmClicked);
     }
 
     private void OnCreateRoomClicked()
@@ -43,26 +33,5 @@ public class LobbyPanel : MonoBehaviour
     private void OnJoinRoomClicked()
     {
         joinRoomPanel.SetActive(true);
-    }
-
-    private void OnConfirmClicked()
-    {
-        string nickname = nicknameInput.text;
-        NetworkManager.Instance.SetNickname(nickname);
-        Debug.Log($"Set Your Nickname : {NetworkManager.Nickname}");
-    }
-    
-    private void UpdateNicknameUI()
-    {
-        string defaultName = String.Empty;
-
-        if (nicknameInput != null)
-        {
-            if (PlayerPrefs.HasKey(PlayerPrefsKeys.playerName))
-            {
-                defaultName = PlayerPrefs.GetString(PlayerPrefsKeys.playerName);
-                nicknameInput.text = defaultName;
-            }
-        }
     }
 }
