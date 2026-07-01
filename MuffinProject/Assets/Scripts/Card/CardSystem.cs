3b4caddc66a776a59568ed26b9458a72ccf2eb98
Roh-HeeGeon <90394227+nhg18@users.noreply.github.com>
2026-07-01 15:30:40 +0900

    add cardsystem pop

--- MuffinProject/Assets/Scripts/Card/CardSystem.cs
+++ MuffinProject/Assets/Scripts/Card/CardSystem.cs
@@ -9,6 +9,8 @@ public class CardSystem : SingletonPun<CardSystem>
 
     Vector3 centerScreen;
     Vector3 spawnPosition;
+
+    private bool isResolutioning = false;
     
 
     private void Start()
@@ -16,6 +18,20 @@ public class CardSystem : SingletonPun<CardSystem>
         centerScreen = new Vector3(Screen.width / 2f, Screen.height / 2f, 0f);
         spawnPosition = Camera.main.ScreenToWorldPoint(centerScreen);
         spawnPosition.z = 0f;
+    }
+
+    private GameObject FindCard(int cardID)
+    {
+        List<GameObject> Cards = PlayerHandsScripts.Instance.Cards;
+        foreach (GameObject prefab in Cards)
+        {
+            CardScript data = prefab.GetComponent<CardScript>();
+            if (data.cardID == cardID)
+            {
+                return prefab;
+            }
+        }
+        return null;
     }
 
     public void RequestPlayCard(int cardID, List<int> targetPlayerNumber)
@@ -30,6 +46,7 @@ public class CardSystem : SingletonPun<CardSystem>
         if (!PhotonNetwork.IsMasterClient) return;
 
         //유효한지 검사
+        if (isResolutioning == true) return;
 
         int casterActorNum = info.Sender.ActorNumber;
 
@@ -39,23 +56,57 @@ public class CardSystem : SingletonPun<CardSystem>
     [PunRPC]
     private void RPC_BroadcastPush(int cardID, int casterActorNum ,int[] targetPlayerNumber)
     {
+        CancelInvoke("StartChainResolution");
         chainList.Add(new ChainItem(cardID, casterActorNum, targetPlayerNumber));
 
         //todo 추가 카운터 카드 받는 코드 실행
-
-        List<GameObject> Cards =  PlayerHandsScripts.Instance.Cards;
-        foreach(GameObject prefab in Cards)
+        GameObject prefab = FindCard(cardID);
+        if(prefab!=null)
         {
-            CardScript data = prefab.GetComponent<CardScript>();
-            if(data.cardID == cardID)
+            GameObject a = Instantiate(prefab, spawnPosition, Quaternion.identity);
+            Destroy(a, 4f);
+        }
+        else
+        {
+            Debug.LogWarning("일치하는 카드 없음");
+        }
+        Invoke("StartChainResolution", 5.0f);
+    }
+
+
+
+    public void StartChainResolution()
+    {
+        isResolutioning = true;
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            photonView.RPC("RPC_ResolveChain", RpcTarget.All);
+        }
+
+
+
+
+        isResolutioning = false;
+    }
+
+    [PunRPC]
+    private void RPC_ResolveChain()
+    {
+        for(int i = chainList.Count - 1; i >= 0; i--)
+        {
+            GameObject popcard = FindCard(chainList[i].cardID);
+            chainList.RemoveAt(0);
+            if (popcard != null)
             {
-                GameObject a = Instantiate(prefab,spawnPosition, Quaternion.identity);
-                Destroy(a, 3f);
-                return;
+                GameObject a = Instantiate(popcard, spawnPosition, Quaternion.identity);
+                Destroy(a, 4f);
+            }
+            else
+            {
+                Debug.LogWarning("일치하는 카드 없음");
             }
         }
-        Debug.LogWarning("일치하는 카드 없음");
-
-
+        chainList.Clear();
     }
 }
