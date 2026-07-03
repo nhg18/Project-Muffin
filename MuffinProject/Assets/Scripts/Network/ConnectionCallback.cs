b2bc91e192277f7cae89388dd68977ce022f4a5d
changhwan.exe <imchanghong15@gmail.com>
2026-07-03 14:58:00 +0900

    Revert "Delete code in NetworkManager"
    This reverts commit 76abe972468ea7c26039579cba4cd25c2222d7e4.

---
+++ MuffinProject/Assets/Scripts/Network/ConnectionCallback.cs
@@ -0,0 +1,59 @@
+using System;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ConnectionCallback
+{
+    private ConnectionService service;
+    public ConnectionCallback(ConnectionService service)
+    {
+        this.service = service;
+    }
+
+    /// <summary>
+    /// 서버 연결 완료시 호출되는 콜백 함수
+    /// 서버 연결시 자동으로 로비 참가
+    /// </summary>
+    public void OnConnectedToMaster()
+    {
+        Debug.Log("On Connected To Master");
+        // PhotonNetwork.JoinLobby(); // 매치 메이킹 없으면 로비 없어도 됨.
+        ConnectionEvents.RaiseConnected(true);
+        PopupManager.Instance.Hide();
+        SceneManager.LoadScene(ScenePaths.Get(SceneType.Lobby));
+    }
+
+    /// <summary>
+    /// 서버 연결 끊어졌을 때 호출되는 콜백 함수
+    /// </summary>
+    /// <param name="cause">
+    /// 끊긴 사유가 담긴 Enum 집합체
+    /// </param>
+    public void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log($"On Disconnected: {cause}");
+        ConnectionEvents.RaiseConnected(false);
+
+        switch (cause)
+        {
+            // 클라이언트에 의해 의도적 종료
+            case DisconnectCause.DisconnectByClientLogic:
+                // SceneManager.LoadScene(SceneType.Title);
+                break;
+
+            // 서버, 클라이언트 타임 아웃, 일시 끊김
+            case DisconnectCause.ServerTimeout:
+            case DisconnectCause.ClientTimeout:
+            case DisconnectCause.Exception:
+                // TryReconnect();
+                break;
+
+            // 서버가 강제 종료 → 이유 표시 후 타이틀
+            case DisconnectCause.DisconnectByServerLogic:
+            case DisconnectCause.InvalidAuthentication:
+                // ShowErrorAndGoTitle(cause);
+                break;
+        }
+    }
+}
---
+++ MuffinProject/Assets/Scripts/Network/RoomService.cs
@@ -0,0 +1,75 @@
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using WebSocketSharp;
+
+public class RoomService
+{
+    /// <summary>
+    /// 룸 참가 함수
+    /// </summary>
+    public void JoinRoom(string roomName)
+    {
+        if (string.IsNullOrEmpty(roomName))
+        {
+            RoomEvents.RaiseJoinRoomFailed(ErrorCode.InvalidOperation, "방 코드가 없습니다.");
+            return;
+        }
+        PhotonNetwork.JoinRoom(roomName.Trim().ToUpper());
+    }
+    
+    /// <summary>
+    /// 랜덤 룸 참가 함수
+    /// </summary>
+    public void JoinRandomRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+    
+    /// <summary>
+    /// 룸 나가기 함수
+    /// </summary>
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+    
+    /// <summary>
+    /// 룸 생성 함수
+    /// </summary>
+    public void CreateRoom()
+    {
+        RoomOptions options = CreateRoomOptions(NetworkManager.MaxPlayers, true, true);
+        string code = RandomCode.GenerateRandomCode();
+        
+        PhotonNetwork.CreateRoom(code.Trim().ToUpper(), options);
+    }
+    
+    /// <summary>
+    /// 룸 옵션 생성 함수
+    /// </summary>
+    /// <param name="maxPlayers">최대 플레이어 수</param>
+    /// <param name="isVisible">로비 노출 여부</param>
+    /// <param name="isOpen">공개 비공개 여부</param>
+    /// <returns>RoomOptions 객체 리턴</returns>
+    public RoomOptions CreateRoomOptions(int maxPlayers, bool isVisible, bool isOpen)
+    {
+        return new RoomOptions
+        {
+            MaxPlayers = maxPlayers,
+            IsVisible = isVisible,
+            IsOpen = isOpen,
+        };
+    }
+    
+    /// <summary>
+    /// 룸 옵션 업데이트 함수
+    /// </summary>
+    /// <param name="isVisible">로비 노출 여부</param>
+    /// <param name="isOpen">공개 비공개 여부</param>
+    public void UpdateRoomOptions(bool isVisible, bool isOpen)
+    {
+        PhotonNetwork.CurrentRoom.IsVisible = isVisible;
+        PhotonNetwork.CurrentRoom.IsOpen = isOpen;
+    }
+}
