ae17357c5b54dcc92a25db221c67d4c768931c06
Roh-HeeGeon <90394227+nhg18@users.noreply.github.com>
2026-07-24 16:38:22 +0900

    bugfix2

--- MuffinProject/Assets/Scripts/Refactor/Hand/PlayerHandView.cs
+++ MuffinProject/Assets/Scripts/Refactor/Hand/PlayerHandView.cs
@@ -49,15 +49,21 @@ public class PlayerHandView : MonoBehaviour
         for (int i = 0; i < Hands.Count; i++)
         {
             float p = firstCardPosition + i * cardSpacing;
-            Vector3 splinePosition = spline.EvaluatePosition(p);
-            Vector3 forward = spline.EvaluateTangent(p);
-            Vector3 up = spline.EvaluateUpVector(p);
-            quaternion rotation = Quaternion.LookRotation(-up, Vector3.Cross(-up, forward).normalized);
 
-            Vector3 worldTarget = splinePosition
-                            + HandPosition.position
+            Vector3 localSplinePos = spline.EvaluatePosition(p);
+            Vector3 worldSplinePos = splineContainer.transform.TransformPoint(localSplinePos);
+
+            Vector3 localForward = spline.EvaluateTangent(p);
+            Vector3 localUp = spline.EvaluateUpVector(p);
+            Vector3 worldForward = splineContainer.transform.TransformDirection(localForward);
+            Vector3 worldUp = splineContainer.transform.TransformDirection(localUp);
+
+            quaternion rotation = Quaternion.LookRotation(-worldUp, Vector3.Cross(-worldUp, worldForward).normalized);
+
+            Vector3 worldTarget = worldSplinePos
                             + 0.01f * i * Vector3.back
                             + new Vector3(0, 0, -i);
+
             Vector3 localPos = HandPosition.InverseTransformPoint(worldTarget);
             Quaternion localRot = Quaternion.Inverse(HandPosition.rotation) * rotation;
 
