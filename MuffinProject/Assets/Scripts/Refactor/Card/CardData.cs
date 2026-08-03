2038c562e1140c3ba5153b68d10dccb22bdb13d6
Roh-HeeGeon <90394227+nhg18@users.noreply.github.com>
2026-08-03 14:33:21 +0900

    cardData type modifed
    change int to enum

--- MuffinProject/Assets/Scripts/Refactor/Card/CardData.cs
+++ MuffinProject/Assets/Scripts/Refactor/Card/CardData.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+public enum Type
+{
+    Action,
+    Counter,
+    Trap
+}
 [CreateAssetMenu(fileName = "Card_", menuName = "CardSystem/Card Data")]
 public class CardData : ScriptableObject
 {
     public int id;
     public string cardName;
     public Sprite cardImage;
-    public int type;
+    public Type type;
 
     [TextArea]
     public string description;
