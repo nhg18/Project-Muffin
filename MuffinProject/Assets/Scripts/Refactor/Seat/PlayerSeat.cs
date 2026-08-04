f220ffc26e06db03ea42cbccd833b4be2edd231c
changhwan.exe <imchanghong15@gmail.com>
2026-08-04 23:05:56 +0900

    턴 표시 이미지 변경

--- MuffinProject/Assets/Scripts/Refactor/Seat/PlayerSeat.cs
+++ MuffinProject/Assets/Scripts/Refactor/Seat/PlayerSeat.cs
@@ -3,11 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerSeat : MonoBehaviour
 {
     [SerializeField] private TMP_Text nicknameText;
+    [SerializeField] private TMP_Text hpText;
+    
     [SerializeField] private TMP_Text myTurnText;
+    
+    [SerializeField] private Image myTurnImage;
+
+    private void Awake()
+    {
+        gameObject.SetActive(false);
+    }
 
     public void SetNicknameUI(string nickname)
     {
@@ -16,6 +26,17 @@ public class PlayerSeat : MonoBehaviour
 
     public void SetTurnUI(bool isTurn)
     {
+        // SetTurnText(isTurn);
+        SetTurnImage(isTurn);
+    }
+
+    private void SetTurnText(bool isTurn)
+    {
         myTurnText.text = isTurn ? "Turn" : "";
+    }
+
+    private void SetTurnImage(bool isTurn)
+    {
+        myTurnImage.enabled = isTurn;
     }
 }
