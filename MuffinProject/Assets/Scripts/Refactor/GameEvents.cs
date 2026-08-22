20c3fb7ec766185ebde738bb87a9b9293603d1d1
changhwan.exe <imchanghong15@gmail.com>
2026-08-22 20:29:24 +0900

    Revert "Init Refactoring"
    This reverts commit b046783abf277fca784a80729bc328d4850d450a.

--- MuffinProject/Assets/Scripts/Refactor/GameEvents.cs
+++ MuffinProject/Assets/Scripts/Refactor/GameEvents.cs
@@ -1,10 +1,8 @@
 using System;
-using Refactor;
 
 public static class GameEvents
 {
     public static event Action OnTurnChanged;
-    public static event Action<Player> OnPlayerDrawn;
 
     public static void RaiseTurnChanged()
     {
